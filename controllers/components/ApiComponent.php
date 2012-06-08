9ad2bbfe1818ffb88e6d71d3a75804a65526b1d4
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2012-06-08 12:22:53 -0400

    ENH: first commit, added skeleton of plugin.

---
+++ controllers/components/ApiComponent.php
@@ -0,0 +1,31 @@
+<?php
+/*=========================================================================
+MIDAS Server
+Copyright (c) Kitware SAS. 20 rue de la Villette. All rights reserved.
+69328 Lyon, FRANCE.
+
+See Copyright.txt for details.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+=========================================================================*/
+
+/** Component for api methods */
+class Slideatlas_ApiComponent extends AppComponent
+{
+
+  /**
+   * Helper function for verifying keys in an input array
+   */
+  private function _checkKeys($keys, $values)
+    {
+    foreach($keys as $key)
+      {
+      if(!array_key_exists($key, $values))
+        {
+        throw new Exception('Parameter '.$key.' must be set.', -1);
+        }
+      }
+    }
+
+} // end class
