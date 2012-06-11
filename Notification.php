4889d9d0a1341a43f958e0ddafbaf2bf4a133f33
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2012-06-11 12:17:13 -0400

    ENH: added model and basic Apis

--- Notification.php
+++ Notification.php
@@ -15,13 +15,35 @@ require_once BASE_PATH . '/modules/api/library/APIEnabledNotification.php';
 class Slideatlas_Notification extends ApiEnabled_Notification
   {
   public $moduleName = 'slideatlas';
-  public $_moduleComponents=array('Api');
+  public $_moduleComponents = array('Api');
   public $_models=array();
 
   /** init notification process*/
   public function init()
     {
+    $this->enableWebAPI($this->moduleName);
+    $fc = Zend_Controller_Front::getInstance();
+    $this->moduleWebroot = $fc->getBaseUrl().'/modules/'.$this->moduleName;
+    $this->coreWebroot = $fc->getBaseUrl().'/core';
+    
+    $this->addCallBack('CALLBACK_CORE_ITEM_DELETED', 'handleItemDeleted');
+    
     }//end init
+    
+
+  /**
+   * When an item is being deleted, we should delete corresponding row in slideatlas_item tables.  
+   */
+  public function handleItemDeleted($params)
+    {
+    $modelLoader = new MIDAS_ModelLoader();
+    $slideatlasItemModel = $modelLoader->loadModel('Item', $this->moduleName);
+    $slideatlasItem = $slideatlasItemModel->getByItemId($params['item']->getKey());
+    if($slideatlasItem)
+      {
+      $slideatlasItemModel->delete($slideatlasItem);
+      }
+    }
   
   } //end class
   
--- controllers/components/ApiComponent.php
+++ controllers/components/ApiComponent.php
@@ -28,4 +28,63 @@ class Slideatlas_ApiComponent extends AppComponent
       }
     }
 
+  /**
+   * Create a slideatlas item or update an existing one. it calls the itemCreate api
+   * to create a regular item and then add slideatals related attributes on top of it.
+   * @param token Authentication token
+   * @param parentid The id of the parent folder. Only required for creating a new item.
+   * @param name The name of the item to create
+   * @param itemOrder(Optional) item order used to sort slideatlas items
+   * @param description (Optional) The description of the item
+   * @param uuid (Optional) Uuid of the item. If none is passed, will generate one.
+   * @param privacy (Optional) Default 'Public', possible values [Public|Private].
+   * @return The item object that was created
+   */
+  public function createItem($args)
+    {
+    $this->_checkKeys(array('parentId', 'name'), $args);
+
+    $componentLoader = new MIDAS_ComponentLoader();
+    $authComponent = $componentLoader->loadComponent('Authentication', 'api');
+    $userDao = $authComponent->getUser($args,
+                                       Zend_Registry::get('userSession')->Dao);
+    if(!$userDao)
+      {
+      throw new Exception('Cannot create item anonymously', MIDAS_INVALID_POLICY);
+      }
+
+    $coreApiComponent = $componentLoader->loadComponent('Api', 'api');
+    $coreItemArray = $coreApiComponent->itemCreate($args);
+
+    $modelLoad = new MIDAS_ModelLoader();
+    $slideatlasItemModel = $modelLoad->loadModel('Item', 'slideatlas');
+    $slideatlasItem = $slideatlasItemModel->createItem($coreItem['item_id'], $args['itemOrder']);
+    $slideatlasItemArray = $slideatlasItem->toArray();
+
+    return ($slideatlasItemArray + $coreItemArray);
+    }
+
+
+  /**
+   * Get an item's information
+   * @param token (Optional) Authentication token
+   * @param id The item id
+   * @param head (Optional) only list the most recent revision
+   * @return The item object
+   */
+  function getItem($args)
+    {
+    $this->_validateParams($args, array('id'));
+
+    $coreApiComponent = $componentLoader->loadComponent('Api', 'api');
+    $coreItemArray = $coreApiComponent->itemGet($args);
+
+    $modelLoader = new MIDAS_ModelLoader();
+    $slideatlasItemModel = $modelLoader->loadModel('Item', 'slideatlas');
+    $slideatlasItem = $slideatlasItemModel->getByItemId($args['id']);
+    $slideatlasItemArray = $slideatlasItem->toArray();
+
+    return ($slideatlasItemArray + $coreItemArray);
+    }
+
 } // end class
